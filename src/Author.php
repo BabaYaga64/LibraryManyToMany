dfa60c123f4d366e1a7a5cdb9f242b9908da49ae
Tanner Heffner & Bojana Skarich <student@epicodus.com>
2015-03-25 11:51:44 -0700

    Add update and delete function to Author class.

--- src/Author.php
+++ src/Author.php
@@ -36,19 +36,45 @@
     //DB FUNCTIONS
         function save()
         {
-
+            $statement = $GLOBALS['DB']->query("INSERT INTO authors (name) VALUES '{$this->getName()}';");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
         }
 
         static function getAll()
         {
+            $returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors;");
+            $authors = array();
 
+            foreach($returned_authors as $author) {
+                $name = $author['name'];
+                $id = $author['id'];
+                $new_author = new Author($name, $id);
+                array_push($authors, $new_author);
+            }
+            return $authors;
+        }
+
+    //UPDATE
+        function updateAuthor($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE authors SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
+    //DELETE
+        function deleteAuthor()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors WHERE id = {$this->getId()};");
         }
 
         static function deleteAll()
         {
-
+            $GLOBALS['DB']->exec("DELETE FROM authors *;")
         }
 
     }
+
+
 
  ?>
