5b505985a0336d8ae2e7ff3a2a8fe297e2214067
tanner heffner & bojana skarich <student@epicodus.com>
2015-03-26 09:48:39 -0700

    Add getAll, save and delete all tests to Copy.php.

--- tests/CopyTest.php
+++ tests/CopyTest.php
@@ -38,67 +38,105 @@
             $this->assertEquals(true, is_numeric($result));
         }
 
-        // function test_save()
-        // {
-        //     //Arrange
-        //     $title = "Hamlet";
-        //     $genre = "Drama";
-        //     $id = null;
-        //     $test_book = new Book($title, $genre, $id);
-        //     $test_book->save();
-        //
-        //     $book_id = $test_book->getId();
-        //     $test_copy = new Copy($book_id, $id2);
-        //
-        //
-        //     //Act
-        //     $test_copy->save();
-        //     $result = Copy::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_copy], $result);
-        // }
+        function test_getBook_Id()
+        {
 
-        // function test_getAll()
-        // {
-        //     //Arrange
-        //     $book_id = 1;
-        //     $id = null;
-        //     $test_copy = new Copy($book_id, $id);
-        //     $book_id2 = 2;
-        //     $id2 = null;
-        //     $test_copy2 = new Copy($book_id2, $id2);
-        //
-        //     //Act
-        //     $test_copy->save();
-        //     $test_copy2->save();
-        //     $result = Copy::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_copy, $test_copy2], $result);
-        // }
-        //
-        // function test_deleteAll()
-        // {
-        //     //Arrange
-        //     $book_id = "Great Gatsby";
-        //     $id = null;
-        //
-        //     $test_copy = new Copy($book_id, $id);
-        //     $book_id2 = "Hamlet";
-        //     $id2 = null;
-        //
-        //     $test_copy2 = new Copy($book_id2, $id2);
-        //     $test_copy->save();
-        //     $test_copy2->save();
-        //
-        //     //Act
-        //     Copy::deleteAll();
-        //     $result = Copy::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([], $result);
-        // }
+            //Arrange
+            $title = "Great Gatsby";
+            $genre = "Adventure";
+            $id = null;
+            $test_book = new Book($title, $genre, $id);
+            $test_book->save();
+
+            $book_id = $test_book->getId();
+            $test_copy = new Copy($id, $book_id);
+            $test_copy->save();
+
+            //Act
+            $result = $test_copy->getBook_Id();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $title = "Hamlet";
+            $genre = "Drama";
+            $id = null;
+            $test_book = new Book($title, $genre, $id);
+            $test_book->save();
+
+            $book_id = $test_book->getId();
+            $test_copy = new Copy($id, $book_id);
+
+
+            //Act
+            $test_copy->save();
+            $result = Copy::getAll();
+
+            //Assert
+            $this->assertEquals([$test_copy], $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $title = "Hamlet";
+            $genre = "Drama";
+            $id = null;
+            $test_book = new Book($title, $genre, $id);
+            $test_book->save();
+
+            $book_id = $test_book->getId();
+            $test_copy = new Copy($id, $book_id);
+
+            $title2 = "Hamlet";
+            $genre2 = "Drama";
+            $id2 = null;
+            $test_book2 = new Book($title2, $genre2, $id2);
+            $test_book2->save();
+
+            $book_id2 = $test_book2->getId();
+            $test_copy2 = new Copy($id2, $book_id2);
+
+
+            //Act
+            $test_copy->save();
+            $test_copy2->save();
+            $result = Copy::getAll();
+
+            //Assert
+            $this->assertEquals([$test_copy, $test_copy2], $result);
+        }
+
+        //Deletes all copies in database
+        function test_deleteAll()
+        {
+            //Arrange
+
+            $title = "Hamlet";
+            $genre = "Drama";
+            $id = null;
+            $test_book = new Book($title, $genre, $id);
+            $test_book->save();
+
+            $book_id = $test_book->getId();
+            $test_copy = new Copy($id, $book_id);
+
+            $test_copy2 = new Copy($id, $book_id);
+            $test_copy->save();
+            $test_copy2->save();
+
+            //Act
+            Copy::deleteAll();
+            $result = Copy::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
 
     }
 
