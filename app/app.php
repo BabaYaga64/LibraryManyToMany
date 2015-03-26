88ca88f8166137038ae4a6a4887710c86daf9258
tanner heffner & bojana skarich <student@epicodus.com>
2015-03-26 12:21:04 -0700

    Add Book class routes.

--- app/app.php
+++ app/app.php
@@ -16,12 +16,57 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
+    //main page
     $app->get("/", function() use($app) {
         return $app['twig']->render('index.twig');
     });
 
-    
+    //show all books
+    $app->get("/books", function() use($app) {
+        return $app['twig']->render('books.twig', array('books' => Book::getAll()));
+    });
 
+    //add a book form goes through this
+    $app->post("/books", function() use($app) {
+        $new_book = new Book($_POST['title'], $_POST['genre']);
+        $new_book->save();
+        return $app['twig']->render('books.twig', array('books' => Book::getAll()));
+    });
+
+    //show one book & all info
+    $app->get("/books/{id}", function($id) use($app) {
+        $current_book = Book::find($id);
+        return $app['twig']->render('a_book.twig', array('book' => $current_book));
+    });
+
+    //edit a single book
+    $app->get("/books/{id}/edit", function($id) use($app) {
+        $current_book = Book::find($id);
+        return $app['twig']->render('book_edit.twig', array('book' => $current_book));
+    });
+
+    //edit form sent as a patch
+    $app->patch("/books/{id}", function($id) use ($app) {
+        $current_book = Book::find($id);
+        $new_title = $_POST['title'];
+        $current_book->update($new_title);
+        return $app['twig']->render('a_book.twig', array('book' => $current_book));
+        });
+
+    //delete a specific book
+    $app->delete("/books/{id}/delete", function($id) use ($app) {
+        $current_book = Book::find($id);
+        $current_book->delete();
+        return $app['twig']->render('books.twig', array('books' => Book::getAll()));
+
+    });
+
+    //delete all books
+        $app->post("/delete_books", function() use ($app) {
+            Book::deleteAll();
+            return $app['twig']->render('books.twig', array('books' => Book::getAll()));
+
+        });
 
     return $app;
 
